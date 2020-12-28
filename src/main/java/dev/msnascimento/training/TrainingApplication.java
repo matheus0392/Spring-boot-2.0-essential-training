32e1f22145100b7d3b1400710569b23552722efc
Matheus Nascimento <mnascimento0392@gmail.com>
2020-12-27 23:00:46 -0300

    using properties

--- src/main/java/dev/msnascimento/training/TrainingApplication.java
+++ src/main/java/dev/msnascimento/training/TrainingApplication.java
@@ -2,11 +2,23 @@ package dev.msnascimento.training;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class TrainingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TrainingApplication.class, args);
+	}
+
+	@RestController
+	@RequestMapping("/api")
+	public class ApiController {
+		@GetMapping("/greeting")
+		public String getGreeting() {
+			return "Hello World from the API";
+		}
 	}
 }
