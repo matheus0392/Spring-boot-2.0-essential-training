2dbeaff2588bd42e168b55ef694acc04fbd8a361
Matheus Nascimento <mnascimento0392@gmail.com>
2020-12-28 22:40:05 -0300

    command line

--- src/main/java/dev/msnascimento/training/RoomController.java
+++ src/main/java/dev/msnascimento/training/RoomController.java
@@ -1,14 +1,10 @@
 package dev.msnascimento.training;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/rooms")
