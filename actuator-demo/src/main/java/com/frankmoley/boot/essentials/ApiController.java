b54a033a03a70f2ff2618e0be4f3578d65e9e88f
Matheus Nascimento <mnascimento0392@gmail.com>
2021-01-03 14:29:17 -0300

    metrics endpoint

--- actuator-demo/src/main/java/com/frankmoley/boot/essentials/ApiController.java
+++ actuator-demo/src/main/java/com/frankmoley/boot/essentials/ApiController.java
@@ -4,29 +4,37 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.micrometer.core.annotation.Timed;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("api")
 public class ApiController {
+	private final Counter greetingsCounter;
 
-    private final PresidentRepository presidentRepository;
+	private final PresidentRepository presidentRepository;
 
-    public ApiController(PresidentRepository presidentRepository){
-        this.presidentRepository = presidentRepository;
-    }
+	public ApiController(PresidentRepository presidentRepository, MeterRegistry registry) {
+		this.presidentRepository = presidentRepository;
+		this.greetingsCounter=Counter.builder("api.greeting").register(registry);
+	}
 
-    @GetMapping("greeting")
-    public String getGreeting(){
-        return "Hello LinkedIn Learning Student";
-    }
+	@GetMapping("greeting")
+	public String getGreeting() {
+		greetingsCounter.increment();
+		return "Hello LinkedIn Learning Student";
+	}
 
-    @GetMapping("presidents")
-    public List<President> getAllUSPresidents(){
-        List<President> presidents = new ArrayList<>();
-        this.presidentRepository.findAll().forEach(presidents::add);
-        return presidents;
-    }
+	@GetMapping("presidents")
+	@Timed(value = "api.getAllPresidents")
+	public List<President> getAllUSPresidents() {
+		List<President> presidents = new ArrayList<>();
+		this.presidentRepository.findAll().forEach(presidents::add);
+		return presidents;
+	}
 
 }
